136b744e92acb97bfa3ef5ed85ce96b15cd6b65e
Sushmita Rudra <sushmitarudra261@gmail.com>
2020-12-28 19:00:51 -0600

    Created a spring boot project and configured application properties.

---
+++ ecommerce-book-store/ecommerce-book-store/src/main/java/com/rudra/ecommercebookstore/EcommerceBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.rudra.ecommercebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBookStoreApplication.class, args);
+	}
+
+}
---
+++ ecommerce-book-store/ecommerce-book-store/src/test/java/com/rudra/ecommercebookstore/EcommerceBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudra.ecommercebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
